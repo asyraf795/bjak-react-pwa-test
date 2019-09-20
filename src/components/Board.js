167c3c70d3791d331a56b3843270ac9b0b0bac46
Mohammad Asyraf Ismail <beta.asyraf95@gmail.com>
2019-09-20 21:44:24 +0800

    BJAK - commit more features

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,16 +1,43 @@
 import React from "react";
 
 import Poster from "./Poster";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
 
 const Board = props => {
+  const responsive=   {
+    0: {
+        items: 3
+    },
+    720: {
+      items: 4
+    },
+    1024: {
+        items: 6
+    },
+    1440: {
+      items: 8
+    }
+}
+
   const posterComponents = props.data.map(poster => (
-    <Poster title={poster.title} images={poster.images} key={poster.id} />
+    <Poster title={poster.title} images={poster.images} key={poster.id} id={poster.id}/>
   ));
+
   return (
-    <div>
+    <div className="container">
       <h2>{props.name}</h2>
-      {posterComponents}
-      <hr></hr>
+      <AliceCarousel
+        responsive={responsive}
+        fadeOutAnimation={true}
+        mouseDragEnabled={true}
+        dotsDisabled={true}
+        infinite={true}
+      >
+        {posterComponents}
+      </AliceCarousel>
+
+      <hr className="dashingred" />
     </div>
   );
 };
--- src/components/Poster.js
+++ src/components/Poster.js
@@ -1,17 +1,22 @@
-// import React, { useState } from "react";
 import React from "react";
+import "./Poster.css"
 
 const Poster = props => {
-
   const filterImage = () => {
-    var result = props.images.filter((image) => image.type === "POSTER");
-    return result
-  }
+    var result = props.images.filter(image => image.type === "POSTER");
+    return result;
+  };
+
+  const href = "https://localhost:3000/movie?id=" + props.id;
+  const handleOnDragStart = e => e.preventDefault()
+
   return (
-    <div>
-      <img src={filterImage()[0].url} alt={props.title}/>
-      <h3>{props.title}</h3>
-    </div>
+    <a href={href} onDragStart={handleOnDragStart}>
+      <div className="imgMax">
+        <img src={filterImage()[0].url} alt={props.title} />
+        <h3 className="fontSize">{props.title}</h3>
+      </div>
+    </a>
   );
 };
 
